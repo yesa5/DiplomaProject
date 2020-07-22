b00f04879227f0216e7d38cbb9623373e65fdea0
yesa5 <eskhatk@gmail.com>
2020-07-23 01:39:25 +0600

    Project refactoring

--- application/routes/api.php
+++ application/routes/api.php
@@ -14,14 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('habbit', 'HabbitController@index');
-Route::get('habbit/{id}', 'HabbitController@show');
-Route::post('habbit', 'HabbitController@store');
-Route::put('habbit/{id}', 'HabbitController@update');
-Route::delete('habbit/{id}', 'HabbitController@destroy');
 
-Route::get('category', 'CategoryController@index');
-Route::get('category/{id}', 'CategoryController@show');
-Route::post('category', 'CategoryController@store');
-Route::put('category/{id}', 'CategoryController@update');
-Route::delete('category/{id}', 'CategoryController@destroy');
