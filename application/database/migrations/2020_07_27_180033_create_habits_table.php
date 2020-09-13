5d65f7f6c106a799b27e2af7f2570b90f8b3af34
yesa5 <eskhatk@gmail.com>
2020-09-13 15:35:32 +0600

    Project refactoring

---
+++ application/database/migrations/2020_07_27_180033_create_habits_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHabitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('habits', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->integer('icon_id');
+            $table->integer('color');
+            $table->integer('duration');
+            $table->bigInteger('category_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('habits');
+    }
+}
